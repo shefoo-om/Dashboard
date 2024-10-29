178c2a60dd05ac1453fe61da79a5490b50ef88a0
omar sherif <gadoomarsh@gmail.com>
2024-10-29 04:07:43 +0300

    finished from the second page

--- src/app/tables/page.tsx
+++ src/app/tables/page.tsx
@@ -1,18 +1,84 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @next/next/no-img-element */
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { Progress } from "@/components/ui/progress"
+import { EllipsisVertical } from "lucide-react"
+
 import icon1 from "@/assets/imgForTable/ch.png"
 import icon2 from "@/assets/imgForTable/ad1.png"
 import icon3 from "@/assets/imgForTable/fi.png"
 import icon4 from "@/assets/imgForTable/spotify.png"
 import icon5 from "@/assets/imgForTable/ad.png"
 
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
-import { Progress } from "@/components/ui/progress"
-import { EllipsisVertical } from "lucide-react"
+import mem1 from "@/assets/imgForTable/member/raymond.jpg"
+import mem2 from "@/assets/imgForTable/member/ressler.png"
+import mem3 from "@/assets/imgForTable/member/kem.png"
+import mem4 from "@/assets/imgForTable/member/glen.png"
+import mem5 from "@/assets/imgForTable/member/funny.png"
+import mem6 from "@/assets/imgForTable/member/bos.png"
 
 const tableOneDetails = [
-  { id: 1 }
+  { id: 1, img: mem1, author: "Raymond Reddington", email: "Raymond@simmmple.com", function: "Manager", type: "Organization", status: "online", history: "23/04/18" },
+  { id: 2, img: mem2, author: "Donald Ressler", email: "Donald@simmmple.com", function: "Programator", type: "Developer", status: "offline", history: "11/01/19" },
+  { id: 3, img: mem3, author: "Tom Keen", email: "Tom@simmmple.com", function: "Programator", type: "Developer", status: "online", history: "19/09/17" },
+  { id: 4, img: mem4, author: "Glen Carter", email: "Glen@simmmple.com", function: "Executive", type: "Projects", status: "online", history: "24/12/08" },
+  { id: 5, img: mem5, author: "Aram", email: "Aram@simmmple.com", function: "Programator", type: "Developer", status: "online", history: "04/10/21" },
+  { id: 6, img: mem6, author: "Harold Cooper", email: "Harlod@simmmple.com", function: "Manager", type: "Organization", status: "offline", history: "14/09/20" },
 ]
-const TableOne = (<></>)
+
+const statusValue = "";
+
+const TableOne = (
+  <Table className="w-full min-w-[550px] h-full rounded-[4px] overflow-auto bg-transparent">
+    <TableHeader>
+      <TableRow>
+        <TableHead className="min-w-[35%] w-max">AUTHOR</TableHead>
+        <TableHead className="w-[12.5%]">FUNCTION</TableHead>
+        <TableHead className="text-center w-[12.5%]">STATUS</TableHead>
+        <TableHead className="text-center w-[12.5%]">EMPLOYED</TableHead>
+        <TableHead className="text-center w-[12.5%]">ACTION</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      {tableOneDetails.map((ele) => (
+        <TableRow key={ele.id} className="border-t-[1px] border-[#222a48]">
+          <TableCell>
+            <div className="w-full flex items-center">
+              <img src={ele.img.src} alt="" className="size-9 rounded-[0.9375rem] bg-transparent mr-3" />
+              <div className="flex flex-col">
+                <div className="text-sm font-bold">{ele.author}</div>
+                <div className="text-xs text-secondColor">{ele.email}</div>
+              </div>
+            </div>
+          </TableCell>
+          <TableCell>
+            <div className="flex flex-col my-2">
+              <p className="text-xs font-bold ">{ele.function}</p>
+              <p className="text-xs font-semibold text-secondColor">{ele.type}</p>
+            </div>
+          </TableCell>
+          <TableCell>
+            <div className="flex">
+              {ele.status === "online" ? <p className="bg-green-600 mx-auto text-xs border-2 border-transparent rounded-lg py-[3px] px-2">{ele.status}</p>
+                :
+                <p className="mx-auto text-xs border-2 rounded-lg py-[2px] px-2">{ele.status}</p>}
+            </div>
+          </TableCell>
+          <TableCell>
+            <div className="flex">
+              <p className="text-xs font-bold mx-auto">{ele.history}</p>
+            </div>
+          </TableCell>
+          <TableCell>
+            <div className="flex">
+              <button className="text-xs mx-auto text-secondColor">Edit</button>
+            </div>
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+)
 
 const tableTwoDetails = [
   { id: 1, icon: icon1, project: "Chakra Vision UI Version", budget: "$14,555", percent: 60, status: "Working" },
@@ -64,19 +130,19 @@ const TableTwo = (
 export default function page() {
   return (
     <div className="w-full flex flex-col justify-between items-center gap-y-5 py-2">
-      <div className="MainCards w-full h-[523.5px] flex flex-col justify-between items-center py-6 px-5">
+      <div className="MainCards w-full min-h-[523.5px] h-max flex flex-col justify-between items-center py-6 px-5">
         <div className=" h-[10%] w-full flex flex-col">
           <p className="headerTitle">Authors table</p>
         </div>
-        <div className="h-[80%] w-full boxShadoooow">
-          asd
+        <div className="h-[88%] w-full boxShadoooow">
+          {TableOne}
         </div>
       </div>
       <div className="MainCards w-full min-h-[377px] h-max flex flex-col justify-between items-center py-6 px-5">
         <div className=" h-[10%] w-full flex flex-col gap-y-1">
           <p className="headerTitle">Projects table</p>
         </div>
-        <div className="h-[88%] w-full boxShadoooow">
+        <div className="h-[89.5%] w-full boxShadoooow">
           {TableTwo}
         </div>
       </div>
