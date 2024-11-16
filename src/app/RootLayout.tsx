fd916686e41fcd4e686e376ec15a59d37e10b54d
omar sherif <gadoomarsh@gmail.com>
2024-11-16 20:30:22 +0200

    update

--- src/app/RootLayout.tsx
+++ src/app/RootLayout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     const pathname = usePathname();
-    const allowedPaths = ["", "profile", "tables", "billing"].map(path => `/${path}`);
+    const allowedPaths = ["", "tables", "billing"].map(path => `/${path}`);
     const route = pathname === "/" || pathname === "" ? "Dashboard" : pathname.replace("/", "");
 
     return (
