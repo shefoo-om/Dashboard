33523b1afecf14a516368f91c3e19482a8302c5a
omar sherif <gadoomarsh@gmail.com>
2024-11-19 11:13:33 +0200

    update the strucutre. install the react hook form, resolves, Zod

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -198,10 +198,10 @@ export default function ProfilePage() {
 
 
 
-      <div className="w-full flex flex-wrap justify-between items-center gap-2">
+      {/* <div className="w-full flex flex-wrap justify-between items-center gap-2">
         <div className="MainCards w-[29%] max-[1440px]:w-full"></div>
         <div className="MainCards w-[69%] max-[1440px]:w-full"></div>
-      </div>
+      </div> */}
 
     </div>
   )
