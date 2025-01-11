d6eb4a177d8d307b32b65392d9ce21157ea8e58b
omar sherif <gadoomarsh@gmail.com>
2025-01-11 09:23:20 +0200

    add the metadata to home page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,12 @@ import FrontCard from "@/components/DashboardPage/frontCard";
 import ChartsCards from "@/components/DashboardPage/ChartsCards";
 import LastCard from "@/components/DashboardPage/LastCard";
 
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Home',
+  description: "the home in page"
+}
 export default function Dashboard() {
   return (
     <div className="pt-2">
